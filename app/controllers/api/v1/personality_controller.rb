f4f03cb8158b3889272e462d5849fd53edbb7e2f
github username <github email address>
2017-09-08 15:29:23 -0400

    displays personality traits

--- app/controllers/api/v1/personality_controller.rb
+++ app/controllers/api/v1/personality_controller.rb
@@ -5,15 +5,20 @@ class Api::V1::PersonalityController < ApplicationController
 
 
   def get_traits
-    content = "sample text"
+    content = input_params
    response = Excon.post("https://gateway.watsonplatform.net/personality-insights/api/v2/profile",
-     :body => content,
+     :body => content[:input],
      :headers => { "Content-Type" => "text/plain" },
      :user => "7d414c9d-43d1-446a-8fe8-abc0189bee45",
      :password => "3XlCsgoqR16O"
    )
    profile = JSON.load(response.body)
    render json: profile
+  end
+
+  private
+  def input_params
+    params.require(:userInput).permit(:input)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-       get '/get_traits', to: "personality#get_traits"
+       # get '/get_traits', to: "personality#get_traits"
+       post '/get_traits', to: "personality#get_traits"
     end
   end
 
