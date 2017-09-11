ecf42691cf5645cbc214dfd88f3b81c74ced713c
github username <github email address>
2017-09-11 18:38:50 -0400

    added more dogs

--- db/data.rb
+++ db/data.rb
@@ -1,4 +1,391 @@
 [
+{"AFFENPINSCHER":
+"Loyal, curious, famously funny; fearless out of all proportion to their size."},
+
+{"AFGHAN HOUND":
+"A breed of charming contradictions: independent and aloof, but sweet and profoundly loyal."},
+
+{"AIREDALE TERRIER":
+"Clever, confident, proud; friendly but courageous."},
+
+{"AKITA":
+"Dignified, courageous, and profoundly loyal."},
+
+{"ALASKAN MALAMUTE":
+"Affectionate, loyal, playful but dignified."},
+
+{"AMERICAN ENGLISH COONHOUND":
+"Sweet, Melow, Sociable"},
+
+{"AMERICAN ESKIMO DOG":
+"Playful, perky, very smart; an excellent trick dog."},
+
+{"AMERICAN FOXHOUND":
+"Sweet-tempered and easygoing, but also stubborn and independent"},
+
+
+{"AMERICAN HAIRLESS TERRIER":
+"Friendly, lovable, inquisitive; fearlessly game for just about anything."},
+
+{"AMERICAN STAFFORDSHIRE TERRIER":
+"Smart, confident, good-natured companions."},
+
+{"AMERICAN WATER SPANIEL":
+"Happy, eager, and charming; aloof with strangers, and a little stubborn."},
+
+{"ANATOLIAN SHEPHERD DOG":
+"Calm, watchful, loyal, and smart; protective and territorial."},
+
+{"AUSTRALIAN CATTLE DOG":
+"Alert, curious, and pleasant."},
+
+{"AUSTRALIAN SHEPHERD":
+"Smart, work-oriented, exuberant—did we mention smart?"},
+
+{"AUSTRALIAN TERRIER":
+"Plucky, spirited, and smart."},
+
+{"BARBET":
+"Friendly, joyful and fun loving"},
+
+{"BASENJI":
+"An intelligent, independent, but affectionate and alert breed. Can be aloof with strangers."},
+
+{"BASSET FAUVE DE BRETAGNE":
+"Smart, courageous and determined"},
+
+{"BASSET HOUND":
+"Easygoing, laid-back, and even a bit lazy, Bassets are great family pets."},
+
+{"BAVARIAN MOUNTAIN SCENT HOUND":
+"quiet, poised, and loyal to their family"},
+
+{"BEAGLE":
+"Beagles are loving and lovable, happy, easygoing, and companionable."},
+
+{"BEARDED COLLIE":
+"Bouncy, charismatic, friendly; smart, but also stubborn and independent."},
+
+{"BEAUCERON":
+"Smart, obedient, gentle; reserved with strangers, loving and eager to please with preferred humans."},
+
+{"BEDLINGTON TERRIER":
+"Rollicking, charming, and full of fun; a loyal member of the family."},
+
+{"BELGIAN MALINOIS":
+"Smart, confident, hardworking, and steadfastly loyal."},
+
+{"BELGIAN SHEEPDOG":
+"Serious-minded, watchful, and very bright and trainable"},
+
+{"BELGIAN TERVUREN":
+"Smart, self-assured, very trainable; affectionate and possessive with loved ones."},
+
+{"BERNESE MOUNTAIN DOG":
+"Good-natured and calm."},
+
+{"BICHON FRISE":
+"Playful, curious, and peppy."},
+
+{"BLACK AND TAN COONHOUND":
+"Easygoing, friendly, bright; mellow by the fireside, rugged and brave on the trail."},
+
+{"BLACK RUSSIAN TERRIER":
+"Smart, confident, courageous; protective of loved ones, aloof with strangers."},
+
+{"BLOODHOUND":
+"Inquisitive, independent, friendly, affectionate with family, can be reserved with strangers"},
+
+{"BLUETICK COONHOUND":
+"A smart and devoted charmer at home; a fearless, tenacious pursuer on the trail."},
+
+{"BORDER COLLIE":
+"Remarkably smart workaholics; not averse to a good cuddle."},
+
+{"BORDER TERRIER":
+"Plucky, happy, affectionate."},
+
+{"BORZOI":
+"Loyal, affectionate, regally dignified; charming but independent-minded."},
+
+{"BOSTON TERRIER":
+"Friendly, bright, amusing, and adaptable."},
+
+{"BOUVIER DES FLANDRES":
+"Strong-willed, even-tempered, courageous; affectionate with family, intimidating to intruders."},
+
+{"BOXER":
+"Fun-loving, bright, active and loyal."},
+
+{"BOYKIN SPANIEL":
+"A classic spaniel: friendly, eager, trainable, and lovable."},
+
+{"BRAQUE FRANCAIS PYRENEAN":
+"friendly, sociable, gentle and submissive"},
+
+{"BRIARD":
+"A whole lot of dog: smart, confident, faithful, with plenty of joie de vivre."},
+
+{"BRITTANY":
+"Bright, upbeat, fun-loving at home; a tireless, enthusiastic worker afield."},
+
+{"BRUSSELS GRIFFON":
+"Alert, curious, and loyal."},
+
+{"BULL TERRIER":
+"Playful and charming; sometimes mischievous, always loyal."},
+
+{"BULLDOG":
+"Calm, courageous, and friendly; dignified but amusing."},
+
+{"BULLMASTIFF":
+"Brave, affectionate, and loyal, the Bullmastiff will always have your back."},
+
+
+{"CAIRN TERRIER":
+"Cheerful, alert, busy and independent-minded."},
+
+{"CANAAN DOG":
+"Alert, vigilant, territorial; docile with family, aloof with strangers."},
+
+{"CANE CORSO":
+"Smart, trainable, and noble of bearing; assertive and confident, Corsi are peerless protectors."},
+
+
+{"CARDIGAN WELSH CORGI":
+"Loyal, affectionate, and smart; even-tempered, never shy."},
+
+
+{"CAVALIER KING CHARLES SPANIEL":
+"Affectionate, graceful, and gentle."},
+
+
+{"CESKY TERRIER":
+"Feisty and tenacious at work, but a bit laid back and trainable than the usual terrier."},
+
+{"CHESAPEAKE BAY RETRIEVER":
+"Bright, sensitive, perceptive; affectionate and family-oriented."},
+
+{"CHIHUAHUA":
+"Graceful, charming, and sassy."},
+
+{"CHINESE CRESTED":
+"Lively, alert, and affectionate."},
+
+{"CHINESE SHAR-PEI":
+"Steadfastly devoted to loved ones, but standoffish and lordly toward strangers."},
+
+
+{"CHINOOK":
+"Patient, smart, willing to please; a devoted family dog."},
+
+
+{"CHOW CHOW":
+"Serious-minded, dignified, bright, and aloof."},
+
+{"CLUMBER SPANIEL":
+"Mellow, gentlemanly, amiable; dignified but always amusing."},
+
+{"COCKER SPANIEL":
+"Happy, smart, gentle."},
+
+{"COLLIE":
+"Graceful, devoted, and proud."},
+
+{"COTON DE TULEAR":
+"Bright, happy-go-lucky, endlessly charming; born to love and to be loved."},
+
+{"CURLY-COATED RETRIEVER":
+"Confident, proud, wickedly smart; a thinking person’s retriever."},
+
+{"DACHSHUND":
+"Go look up the word spunky. We’ll wait. Did you see a photo of a Dachsie? We thought so."},
+
+{"DALMATIAN":
+"A well-bred Dal is stable and outgoing; dignified but never shy."},
+
+{"DANDIE DINMONT TERRIER":
+"Independent, proud, smart; affectionate at home, bold and tenacious in the field."},
+
+{"DOBERMAN PINSCHER":
+"Alert, fearless, loyal, and highly trainable."},
+
+{"DOGUE DE BORDEAUX":
+"Loyal, affectionate, and protective; courageous but not aggressive."},
+
+{"DUTCH SHEPHERD":
+"lively, athletic, alert and intelligent"},
+
+{"ENGLISH COCKER SPANIEL":
+"Merry and responsive, alive with energy."},
+
+{"ENGLISH FOXHOUND":
+"The prototypical pack hound: gentle, affectionate, and sociable."},
+
+{"ENGLISH SETTER":
+"Friendly, merry, and mellow; called the “gentleman of the dog world.”"},
+
+
+{"ENGLISH SPRINGER SPANIEL":
+"Friendly, playful, obedient at home; hardworking and steady in the field."},
+
+{"ENGLISH TOY SPANIEL":
+"A true spaniel—affectionate, alert, and merry."},
+
+{"ENTLEBUCHER MOUNTAIN DOG":
+"Enthusiastic, loyal, smart; confident but not aggressive."},
+
+{"FIELD SPANIEL":
+"Sweet, sensitive, fun-loving, and smart; a real people-pleaser."},
+
+{"FINNISH LAPPHUND":
+"Reserved with strangers, but usually calm and friendly; submissive at home, courageous at work."},
+
+{"FINNISH SPITZ":
+"Eager, courageous, friendly, and faithful."},
+
+{"FLAT-COATED RETRIEVER":
+"Happy, confident, intelligent, and forever young."},
+
+{"FRENCH BULLDOG":
+"Playful, smart, adaptable, and completely irresistible."},
+
+{"GERMAN PINSCHER":
+"Intelligent, determined, assertive; playful and vivacious into old age."},
+
+{"GERMAN SHEPHERD DOG":
+"Smart, confident, courageous, and steady; a true dog lovers dog."},
+
+{"GERMAN SHORTHAIRED POINTER":
+"Friendly, smart, willing to please."},
+
+{"GERMAN WIREHAIRED POINTER":
+"Affectionate, eager, enthusiastic; aloof but not unfriendly with strangers."},
+
+{"GIANT SCHNAUZER":
+"Loyal, alert, trainable, with the stable temperament required in big dogs of great strength."},
+
+{"GLEN OF IMAAL TERRIER":
+"Gentler, less excitable than most terriers, but still bold and spirited."},
+
+{"GOLDEN RETRIEVER":
+"Intelligent, friendly, and devoted."},
+
+{"GORDON SETTER":
+"Bold, confident, and resolute afield; sweetly affectionate by the fireside."},
+
+{"GREAT DANE":
+"Friendly, patient, & dependable."},
+
+{"GREAT PYRENEES":
+"Calm, patient, smart; strong-willed yet affectionate."},
+
+{"GREATER SWISS MOUNTAIN DOG":
+"Faithful, dependable, family oriented; Swissies are famously even-tempered."},
+
+{"GREYHOUND":
+"Independent, gentle, noble, and oh so sweet, but intense when on the run."},
+
+
+{"HARRIER":
+"Outgoing and friendly; true pack hounds, they generally get on well with other dogs."},
+
+{"HAVANESE":
+"Outgoing, funny, & intelligent."},
+
+
+{"HOKKAIDO":
+"Brave, Devoted, Intelligent"},
+
+
+{"IBIZAN HOUND":
+"Even-tempered and polite; a bit aloof, but a sweet-natured, family-oriented housemate."},
+
+{"ICELANDIC SHEEPDOG":
+"Confident, cheerful, sociable; the expression “man’s best friend” was coined for dogs like this."},
+
+{"IRISH RED AND WHITE SETTER":
+"Fun-loving, friendly, high-spirited; a real handful if bored or neglected."},
+
+
+{"IRISH SETTER":
+"Outgoing, sweet-natured, active, and trainable."},
+
+{"IRISH TERRIER":
+"Bold, dashing, even reckless; stouthearted at work, tenderhearted at home."},
+
+{"IRISH WATER SPANIEL":
+"Alert and inquisitive; hardworking and brave in the field, playful and affectionate at home."},
+
+{"IRISH WOLFHOUND":
+"Calm, dignified, kindly; courageous but not aggressive"},
+
+{"ITALIAN GREYHOUND":
+"Sensitive and alert, playful, highly affectionate"},
+
+{"JAPANESE CHIN":
+"A true aristocrat: charming and loving with those they favor, nobly reserved with strangers"},
+
+{"JINDO":
+"Independent, Hunter, Discerning Guardian"},
+
+{"KEESHOND":
+"Outgoing, lively, friendly to all; a lover, not a fighter"},
+
+{"KERRY BLUE TERRIER":
+"Smart and alert; people oriented, with a dash of Irish humor in his heart"},
+
+{"KOMONDOR":
+"Dignified, brave, protective, steadfastly loyal"},
+
+{"KUVASZ":
+"Intensely loyal, protective, utterly fearless; sweet and patient with family"},
+
+{"LABRADOR RETRIEVER":
+"Friendly and outgoing, Labs play well with others"},
+
+{"LAKELAND TERRIER":
+"Brave, bright, confident; affectionate with family, especially kids"},
+
+{"LAPPONIAN HERDER":
+"independent and fun"},
+
+{"LEONBERGER":
+"Bright, patient, loving, and family oriented"},
+
+{"LHASA APSO":
+"Smart, confident, complex; a family comedian, but regally aloof with strangers"},
+
+{"LOWCHEN":
+"Affectionate, outgoing, strong willed, and brave as a lion."},
+
+{"MALTESE":
+"Gentle, playful, affectionate; fearless in a charming toy-dog way."},
+
+{"MANCHESTER TERRIER":
+"Spirited, bright, keenly observant; dead loyal, in an independent terrier way."},
+
+{"MASTIFF":
+"Courageous and dignified, but good-natured and docile."},
+
+{"MINIATURE AMERICAN SHEPHERD":
+"Good-natured, Devoted, Intelligent"},
+
+{"MINIATURE BULL TERRIER":
+"Upbeat, mischievous, comical, with terrier fire and fearlessness."},
+
+{"MINIATURE PINSCHER":
+"Proud, fearless, and fun-loving, Min Pins are a true personality breed."},
+
+{"MINIATURE SCHNAUZER":
+"Friendly, smart, obedient."},
+
+{"MOUNTAIN CUR":
+"Strong-willed, reserved with strangers"},
+
+{"MUDI":
+"active, intelligent, biddable working breed"},
+
   {"NEAPOLITAN MASTIFF":
   "Watchful and dignified; sweet, steady, and loyal with loved ones, calm but wary with strangers."},
 
